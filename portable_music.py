380da2b0566d364a7897809e6da7a0c8c5bcf849
koharuLab <pomme261725@gmail.com>
2025-05-06 01:41:39 +0900

    Update portable_music.py

--- portable_music.py
+++ portable_music.py
@@ -40,7 +40,7 @@ if uploaded_file is not None:
     # 画像のサイズから中央正方形を計算し、切り抜く
     width, height = original_image.size
     factor = 0.7
-    side = min(width, height)
+    side = int(min(width, height) * factor)
     left = (width - side) // 2
     top = (height - side) // 2
     cropped_image = original_image.crop((left, top, left + side, top + side))
